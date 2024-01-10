9deee90783338e77f3aa07e5601dd86cd4862446
Jemish1312 <jemishrupapara02@gmail.com>
2024-01-10 17:25:09 -0500

    Initial Commit

---
+++ IOS project/IOS_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_projectApp.swift
+//  IOS project
+//
+//  Created by user237707 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
